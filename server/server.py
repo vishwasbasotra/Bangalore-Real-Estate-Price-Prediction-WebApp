a0af19f81ecbb0250b96311d8eb9f9cfbcd7ff8d
Vishwas Basotra <brigu.sharma.9@gmail.com>
2020-06-13 16:26:13 +0530

    flask backend done

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request,jsonify
 import util
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
